d5c92d5b0efe64280ab8efdb871bda5482e73c47
Adonaï Elohim Exaucé <adonainangui03@gmail.com>
2019-10-17 15:16:12 +0000

    Update functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,12 +1,3 @@
-(function(open) {
-    XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
-        this.addEventListener("load", function() {
-            handleDatasFromUrl(url, this)
-        }, false);
-        open.call(this, method, url, async, user, pass);
-    };
-})(XMLHttpRequest.prototype.open);
-
 function extractDatas(tree, jsonResult) {
     let returned = {}
     result = JSON.parse(jsonResult)
@@ -21,22 +12,19 @@ function handleDatasFromUrl(url, request) {
     let splitted = url.split('?')
     let datas = {}
     switch (splitted[0]) {
-        case 'https://gaindeintegral.douanes.sn/gainde/manifesteGrid.action':
+        case '/gainde/manifesteGrid.action':
             datas = extractDatas({
-                listeManifeste: [],
-                records: 0,
-                total: 0,
-                rows: 20
+                listeManifeste: []
             }, request.responseText)
             sendRequest('endpoint', datas)
             break;
-        case 'https://gaindeintegral.douanes.sn/gainde/listEnregArtEmbarquementGridAction.action':
+        case '/gainde/listEnregArtEmbarquementGridAction.action':
             datas = extractDatas({
                 listeLieuEmbarqGrid: []
             }, request.responseText)
             sendRequest('endpoint', datas)
             break;
-        case 'https://gaindeintegral.douanes.sn/gainde/listArtEnregGridAction.action':
+        case '/gainde/listArtEnregGridAction.action':
             datas = extractDatas({
                 listeArtParLieuEmbarqGrid: []
             }, request.responseText)
@@ -52,9 +40,15 @@ function sendRequest(endpoint, datas) {
         method: 'POST',
         body: JSON.stringify(datas)
     }).then((response) => {
-        let nextButton = document.getElementById("next-button")
-        if (nextButton) {
-            nextButton.click()
-        }
+       console.log(response)
     })
 }
+
+(function(open) {
+    XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
+        this.addEventListener("load", function() {
+            handleDatasFromUrl(url, this)
+        }, false);
+        open.call(this, method, url, async, user, pass);
+    };
+})(XMLHttpRequest.prototype.open);
