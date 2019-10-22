0538f66355373730d6b40d336ba0704353034647
Mohamed Johnson <mohamedjohnson31@gmail.com>
2019-10-22 10:22:11 +0000

    Starting Logic Implementation

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -33,27 +33,91 @@ class DataController extends Controller
         $fileToRead = Storage::get($file);
         $fileContent = json_decode($fileToRead, true);
 
-        $manifest_data = $fileContent['listeManifeste'];
+        // $manifest_data = $fileContent['listeManifeste'];
 
-        dd($fileContent);
+        // dd($fileContent);
 
-        foreach ($manifest_data as $items) {
-            if (is_array($items)) {
-                foreach($items as $data) {
-                    echo 'Array <br>';
-                    print_r($data);
+        foreach ($fileContent as $bls) {
+            highlight_string(json_encode($bls, JSON_PRETTY_PRINT));
+
+            // Check if the destination is Senegal or Mali
+            if (strtoupper($bls['pays_dest']) === 'SN' || strtoupper($bls['pays_dest']) === 'ML') {
+
+                // Check if the bl has containers
+                if(!empty($bls['conteneurs'])) {
+                    $bltype = $this->getBlType($bls['conteneurs'][0]);
+
+                    echo $twenty_feets = $this->get20s($bls['conteneurs']);
+
+                    echo $fourty_feets = $this->get40s($bls['conteneurs']);
                 }
 
-                echo 'String <br>';
-                print_r($items);
             }
 
-            // echo $items;
+            foreach($bls as $content) {
+                // highlight_string(json_encode($content, JSON_PRETTY_PRINT));
+                // if(is_array($content)) {
+                //     var_export($content);
+                // }
+
+                // if(empty($content['contenesurs'])) {
+                    // print_r($content);
+                // }
+            }
         }
 
         // dd($needed_data);
 
         // return view('gainde.index', ['content' => $needed_data]);
+    }
+
+    /**
+     * Get bl type through first container.
+     *
+     * @param Array $firstContainer
+     * @return Bltype
+     */
+    public function getBlType($firstContainer)
+    {
+        return strtolower(substr($firstContainer[3], 0, 2)) === 're' ? 'REEFER' : 'DRY'  ;
+    }
+
+    /**
+     * Get number of 20 feet containers.
+     *
+     * @param Array $blContainers
+     * @return Number
+     */
+    public function get20s($blContainers)
+    {
+        $n_20 = 0;
+
+        foreach ($blContainers as $container) {
+            if (substr($container[2], 0, 2) === '20') {
+                $n_20++;
+            }
+        }
+
+        return $n_20;
+    }
+
+    /**
+     * Get number of 40 feet containers.
+     *
+     * @param Array $blContainers
+     * @return Number
+     */
+    public function get40s($blContainers)
+    {
+        $n_40 = 0;
+
+        foreach ($blContainers as $container) {
+            if (substr($container[2], 0, 2) !== '20') {
+                $n_40++;
+            }
+        }
+
+        return $n_40;
     }
 
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(LoadingSeeder::class);
-        $this->call(ShippingsTableSeeder::class);
+        // $this->call(ShippingsTableSeeder::class);
     }
 }
