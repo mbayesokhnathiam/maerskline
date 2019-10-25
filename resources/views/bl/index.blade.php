31ddc59725be0e63b392ac50e67561589f9fdb9f
Mohamed Johnson <mohamedjohnson31@gmail.com>
2019-10-25 20:15:48 +0000

    Bl Design Updated

--- resources/views/bl/index.blade.php
+++ resources/views/bl/index.blade.php
@@ -3,48 +3,42 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-10">
-                <div class="d-flex justify-content-center align-items-center">
-                    <div class="d-flex w-75 align-items-start justify-content-between mb-2 px-3 py-3">
-                        <h1>Delivery Slips</h1>
-                        <a href="{{ url('/export/bls/excel') }}" class="btn btn-outline-success font-weight-bold rounded-0">
-                            Exporter
-                            <i class="fas fa-file-excel ml-2"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="mt-3 w-100">
-                    @foreach ($bls as $bl)
-                    <div class="d-flex justify-content-center align-items-center">
-                        <div class="bg-white shadow-sm d-flex flex-column w-75 align-items-start justify-content-between mb-2 px-3 py-3">
-                            <div class="pl-2">
-                                <h5 class="font-weight-bolder ">Nᵒ {{ $bl->bl_number }}</h5>
-
-                                <div class="d-flex justify-content-between py-3">
-                                    <div class="mr-5">
-                                        <h5 class="font-weight-bold">Cargo Type</h5>
-                                        <h5 class="font-weight-bold">Owner</h5>
-                                        <h5 class="font-weight-bold">Shipper</h5>
-                                        <h5 class="font-weight-bold">Commodity</h5>
-                                    </div>
-
-                                    <div>
-                                        <h5>{{ $bl->cargo_type }}</h5>
-                                        <h5>{{ $bl->order }}</h5>
-                                        <h5>{{ $bl->shipper }}</h5>
-                                        <h5>{{ $bl->commodity }}</h5>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="d-flex justify-content-between pl-2 rounded-0">
-                                <a class="btn bg-maersk-primary font-weight-bold p-2 px-4 h5 shadow-sm text-white rounded-0" href="{{ url('/bls/details/' . $bl->id) }}">Details</a>
-                            </div>
+                <div class="d-flex justify-content-center align-items-center w-100">
+                        <div class="d-flex w-100  align-items-start justify-content-between mb-2 px-3 py-3">
+                            <h1>Delivery Slips</h1>
+                            <a href="{{ url('/export/bls/excel') }}" class="btn btn-outline-success font-weight-bold rounded-0">
+                                Exporter
+                                <i class="fas fa-file-excel ml-2"></i>
+                            </a>
                         </div>
                     </div>
-                    @endforeach
-                </div>
-                <div class="d-flex align-items-center justify-content-center w-100">
+            <div class="col-md-12 overflow-auto">
+                <table class="table table-striped shadow">
+                    <thead>
+                        <tr>
+                            <th>Nᵒ</th>
+                            <th>Cargo Type</th>
+                            <th>Owner</th>
+                            <th>Shipper</th>
+                            <th>Commodity</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($bls as $bl)
+                            <tr>
+                                <td class="align-middle">{{ $bl->bl_number }}</td>
+                                <td class="align-middle">{{ $bl->cargo_type }}</td>
+                                <td class="align-middle">{{ $bl->order }}</td>
+                                <td class="align-middle">{{ $bl->shipper }}</td>
+                                <td class="align-middle">{{ $bl->commodity }}</td>
+                                <td class="align-middle"><a class="btn bg-maersk-primary font-weight-bold p-2 px-4 h5 shadow-sm text-white rounded-0" href="{{ url('/bls/details/' . $bl->id) }}">Details</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="d-flex align-items-center justify-content-center mt-3 w-100">
                     {{ $bls->links() }}
                 </div>
             </div>
